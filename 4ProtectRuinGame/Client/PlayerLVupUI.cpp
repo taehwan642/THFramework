2fad2f71455f85fde3ee4b48a70747d8ac4a8b5c
Taehwan Kim <49132911+taehwan642@users.noreply.github.com>
2021-08-10 23:32:51 +0900

    Updating

---
+++ 4ProtectRuinGame/Client/PlayerLVupUI.cpp
@@ -0,0 +1,2 @@
+#include "DXUT.h"
+#include "PlayerLVupUI.h"
--- 4ProtectRuinGame/Client/PlayerLVupUI.h
+++ 4ProtectRuinGame/Client/PlayerLVupUI.h
@@ -1,5 +1,25 @@
 #pragma once
+#include "Sprite.h"
+
+enum class StatUp
+{
+	HPUP,
+	ATTACKSPEEDUP,
+	SPEEDUP,
+	ATTACKUP
+};
+
+struct StatUpUI :
+	public Sprite
+{
+	StatUp statup;
+	void Update() override {};
+};
+
 class PlayerLVupUI
 {
+	StatUpUI* statupUI[3];
+	// statupUI 3개를 화면에 띄우고, 선택한 statupUI 속 statup에 기록된 정보를 토대로 플레이어의 능력을 업그레이드 시킨다.
+	
 };
 
